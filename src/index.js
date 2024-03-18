92fb15a22d9e2bd82f197f578d5d1e83e0f635e5
JulianaAmoasei <late.parrot@gmail.com>
2024-03-18 16:38:40 -0300

    video: lendo arquivos com fs

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
+const fs = require('fs');
+
 const caminhoArquivo = process.argv;
 const link = caminhoArquivo[2]; 
 
-console.log(link);
+fs.readFile(link, 'utf-8', (erro, texto) => {
+  console.log(texto);
+})
