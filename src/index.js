fa294fe51307970c0be08e6995feecd838243354
JulianaAmoasei <late.parrot@gmail.com>
2024-03-18 17:49:04 -0300

    video: capturando as palavras

--- src/index.js
+++ src/index.js
@@ -4,5 +4,24 @@ const caminhoArquivo = process.argv;
 const link = caminhoArquivo[2]; 
 
 fs.readFile(link, 'utf-8', (erro, texto) => {
-  console.log(texto);
+  verificaPalavrasDuplicadas(texto);
 })
+
+// criar um array com as palavras
+// contar as ocorrências
+// montar um objeto com o resultado
+
+// {
+//   "web": 5,
+//   "computador": 4
+// }
+
+function verificaPalavrasDuplicadas(texto) {
+  const listaPalavras = texto.split(' ');
+  const resultado = {};
+  // objeto[propriedade] = valor;
+  listaPalavras.forEach(palavra => {
+    resultado[palavra] = (resultado[palavra] || 0) + 1
+  })
+  console.log(resultado);
+}
