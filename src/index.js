99c16997c2455042700e4459b0e128fff03fc36f
JulianaAmoasei <late.parrot@gmail.com>
2024-03-19 15:24:34 -0300

    video: importação e exportação em JS

--- src/index.js
+++ src/index.js
@@ -1,19 +1,4 @@
-const fs = require('fs');
-const trataErros = require('./erros/funcoesErro');
-
-const caminhoArquivo = process.argv;
-const link = caminhoArquivo[2]; 
-
-fs.readFile(link, 'utf-8', (erro, texto) => {
-  try {
-    if (erro) throw erro
-    contaPalavras(texto);
-  } catch(erro) {
-    trataErros(erro);
-  }
-})
-
-function contaPalavras(texto) {
+export function contaPalavras(texto) {
   const paragrafos = extraiParagrafos(texto)
   const contagem = paragrafos.flatMap((paragrafo) => {
     if (!paragrafo) return [];
