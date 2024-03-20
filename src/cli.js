b23c01fe9f3ad9c9a087c2a6581b0e43ede7fb73
JulianaAmoasei <late.parrot@gmail.com>
2024-03-20 14:06:07 -0300

    video: implementando libs externas

--- src/cli.js
+++ src/cli.js
@@ -1,4 +1,5 @@
 import fs from 'fs';
+import path from 'path';
 import trataErros from './erros/funcoesErro.js';
 import { contaPalavras } from './index.js';
 import { montaSaidaArquivo } from './helpers.js';
@@ -18,21 +19,32 @@ program
       program.help();
       return;
     }
+
+    const caminhoTexto = path.resolve(texto);
+    const caminhoDestino = path.resolve(destino);
+
+    try {
+      processaArquivo(caminhoTexto, caminhoDestino);
+      console.log('texto processado com sucesso');
+    } catch (erro) {
+      console.log('ocorreu um erro no processamento', erro);
+    }
   })
 
-const caminhoArquivo = process.argv;
-const link = caminhoArquivo[2];
-const endereco = caminhoArquivo[3];
+program.parse();
 
-fs.readFile(link, 'utf-8', (erro, texto) => {
-  try {
-    if (erro) throw erro
-    const resultado = contaPalavras(texto);
-    criaESalvaArquivo(resultado, endereco)
-  } catch(erro) {
-    trataErros(erro);
-  }
-})
+function processaArquivo(texto, destino) {
+  fs.readFile(texto, 'utf-8', (erro, texto) => {
+    try {
+      if (erro) throw erro
+      const resultado = contaPalavras(texto);
+      criaESalvaArquivo(resultado, destino)
+    } catch(erro) {
+      trataErros(erro);
+    }
+  })
+}
+
 
 async function criaESalvaArquivo(listaPalavras, endereco) {
   const arquivoNovo = `${endereco}/resultado.txt`;
