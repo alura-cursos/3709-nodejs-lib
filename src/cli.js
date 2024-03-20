03b73d03c87f50f7d39b56335da41559c8856221
JulianaAmoasei <late.parrot@gmail.com>
2024-03-20 12:05:24 -0300

    video: instalando libs externas

--- src/cli.js
+++ src/cli.js
@@ -2,6 +2,23 @@ import fs from 'fs';
 import trataErros from './erros/funcoesErro.js';
 import { contaPalavras } from './index.js';
 import { montaSaidaArquivo } from './helpers.js';
+import { Command } from 'commander';
+
+const program = new Command();
+
+program
+  .version('0.0.1')
+  .option('-t, --texto <string>', 'caminho do texto a ser processado')
+  .option('-d, --destino <string>', 'caminho da pasta onde salvar o arquivo de resultados')
+  .action((options) => {
+    const { texto, destino } = options;
+
+    if (!texto || !destino) {
+      console.error('erro: favor inserir caminho de origem e destino')
+      program.help();
+      return;
+    }
+  })
 
 const caminhoArquivo = process.argv;
 const link = caminhoArquivo[2];
