6b202130d12ca9c86e475b2a3b62c3af355b220f
JulianaAmoasei <late.parrot@gmail.com>
2024-03-20 11:48:50 -0300

    video: adicionando funcionalidades

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,7 @@
 import fs from 'fs';
 import trataErros from './erros/funcoesErro.js';
 import { contaPalavras } from './index.js';
+import { montaSaidaArquivo } from './helpers.js';
 
 const caminhoArquivo = process.argv;
 const link = caminhoArquivo[2];
@@ -18,7 +19,7 @@ fs.readFile(link, 'utf-8', (erro, texto) => {
 
 async function criaESalvaArquivo(listaPalavras, endereco) {
   const arquivoNovo = `${endereco}/resultado.txt`;
-  const textoPalavras = JSON.stringify(listaPalavras);
+  const textoPalavras = montaSaidaArquivo(listaPalavras);
   try {
     await fs.promises.writeFile(arquivoNovo, textoPalavras);
     console.log('arquivo criado');
