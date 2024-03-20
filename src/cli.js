184f5747227cddc815099fc1c916f45de187b099
JulianaAmoasei <late.parrot@gmail.com>
2024-03-20 14:20:28 -0300

    video: decorando o terminal

--- src/cli.js
+++ src/cli.js
@@ -4,6 +4,7 @@ import trataErros from './erros/funcoesErro.js';
 import { contaPalavras } from './index.js';
 import { montaSaidaArquivo } from './helpers.js';
 import { Command } from 'commander';
+import chalk from 'chalk';
 
 const program = new Command();
 
@@ -15,7 +16,7 @@ program
     const { texto, destino } = options;
 
     if (!texto || !destino) {
-      console.error('erro: favor inserir caminho de origem e destino')
+      console.error(chalk.red('erro: favor inserir caminho de origem e destino'))
       program.help();
       return;
     }
@@ -25,7 +26,7 @@ program
 
     try {
       processaArquivo(caminhoTexto, caminhoDestino);
-      console.log('texto processado com sucesso');
+      console.log(chalk.green('texto processado com sucesso'));
     } catch (erro) {
       console.log('ocorreu um erro no processamento', erro);
     }
